594821caf70ad45eb09a45b77af30436103e28d8
Varenya Thyagaraj <varen90@gmail.com>
2023-08-25 11:48:27 +0530

    add typedef for new process variables

--- src/environment-config/runtime-config.ts
+++ src/environment-config/runtime-config.ts
@@ -2,9 +2,21 @@ type RuntimeConfig = {
   variable1: string;
   variable2: string;
 };
+
+// add new process env types to typedef
+
+declare global {
+  namespace NodeJS {
+    export interface ProcessEnv {
+      RUNTIME_VARIABLE_1: string;
+      RUNTIME_VARIABLE_2: string;
+    }
+  }
+}
+
 const runtimeConfig: RuntimeConfig = {
-  variable1: process.env.RUNTIME_VARIABLE_1!,
-  variable2: process.env.RUNTIME_VARIABLE_2!,
+  variable1: process.env.RUNTIME_VARIABLE_1,
+  variable2: process.env.RUNTIME_VARIABLE_2,
 };
 
 export type { RuntimeConfig };
