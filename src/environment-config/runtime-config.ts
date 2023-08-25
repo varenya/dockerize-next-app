3d0c1039808947b4151ab7e18db039587b79d5f6
Varenya Thyagaraj <varen90@gmail.com>
2023-08-25 13:08:30 +0530

    using zod to add validation

--- src/environment-config/runtime-config.ts
+++ src/environment-config/runtime-config.ts
@@ -1,16 +1,20 @@
+import { z } from "zod";
+
 type RuntimeConfig = {
   variable1: string;
   variable2: string;
 };
 
+const runtimeConfigSchema = z.object({
+  RUNTIME_VARIABLE_1: z.string().min(1),
+  RUNTIME_VARIABLE_2: z.string().max(20),
+});
+
 // add new process env types to typedef
 
 declare global {
   namespace NodeJS {
-    export interface ProcessEnv {
-      RUNTIME_VARIABLE_1: string;
-      RUNTIME_VARIABLE_2: string;
-    }
+    interface ProcessEnv extends z.infer<typeof runtimeConfigSchema> {}
   }
 }
 
@@ -18,6 +22,11 @@ const runtimeConfig: RuntimeConfig = {
   variable1: process.env.RUNTIME_VARIABLE_1,
   variable2: process.env.RUNTIME_VARIABLE_2,
 };
+
+// avoid running this on browser as process.env will be undefined there
+if (typeof window === "undefined") {
+  runtimeConfigSchema.parse(process.env);
+}
 
 export type { RuntimeConfig };
 
